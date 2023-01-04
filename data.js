5f50c2e866d0a89189cea7508173fd702ecc98cc
rdgupta93 <110840567+rdgupta93@users.noreply.github.com>
2023-01-04 21:29:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const data =[
+    {name:'Ram gupta',email:'rdgupta@gmail.com',contact:'99483'},
+    {name:'Sam',email:'sam@gmail.com',contact:'76436'},
+    {name:'peter',email:'peter@gmail.com',contact:'738267'}
+]
+module.exports=data;
