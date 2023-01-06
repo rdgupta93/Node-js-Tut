dacdec237f0195f8d32e974118ec51ccc0f85c45
rdgupta93 <110840567+rdgupta93@users.noreply.github.com>
2023-01-06 22:36:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require('http');
-const data = require('./data');
+const fs = require('fs');
 
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(4600);
-    
+const input = process.argv;
+
+if(input[2]=='add')
+{
+fs.writeFileSync(input[3],input[4])
+}else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3])
+}else{
+    console.log("Invalid output")
+}
